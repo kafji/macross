e77cf25bd4771027d66b2a4681b4232c861e3980
Kafji <k@kafji.net>
2022-09-17 22:09:49 +0700

    fix include_micration requires unused arg

--- src/sqlite_migration.rs
+++ src/sqlite_migration.rs
@@ -3,7 +3,7 @@ use indoc::formatdoc;
 
 #[macro_export]
 macro_rules! include_migration {
-    ($dir:expr, $name:expr) => {
+    ($name:expr) => {
         include_str!(concat!(
             env!("CARGO_MANIFEST_DIR"),
             concat!(concat!("/migration/", $name), ".sql")
